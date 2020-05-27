c3ac334bab48ee0cf57ce018ba525ed6778ea028
xian.luo <xian.luo@ly.com>
2020-05-27 14:13:01 +0800

    first commit

---
+++ src/main/java/com/luoxian/alg/AlgApplication.java
@@ -0,0 +1,13 @@
+package com.luoxian.alg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luoxian/alg/AlgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luoxian.alg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
